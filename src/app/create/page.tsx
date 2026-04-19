92ec51d8717ae747e317437c91cb6148881e5a84
Christopher Wetherill <git@tbmh.org>
2026-04-19 15:36:53 -0400

    remove unused file

--- src/app/create/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CreatePage() {
-  return (
-    <div>
-      <h1>This is the Create Page (Placeholder)</h1>
-      <p>The content of the Create Event Form has been moved to a shared component.</p>
-    </div>
-  );
-}
